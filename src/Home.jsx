7fe18fa3faa67c1ac3ca9375623f0ad48af6e1ba
zachlengel <156560805+zachlengel@users.noreply.github.com>
2024-05-01 15:30:33 -0400

    added animated homepage

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,10 @@
 export function Home() {
   return (
     <div className="homepage">
-      <h1>Welcome to GrowPro!</h1>
-      <p>
-        Get started by creating an account and logging in. <br /> <br /> Next you can browse our collection of plants
-        and add them to your watering schedule! <br /> <br /> Turning your thumb green since 2024!
-      </p>
+      <h1 className="transition">Welcome to GrowPro!</h1>
+      <p className="p1">Get started by creating an account and logging in.</p>
+      <p className="p2"> Next you can browse our collection of plants and add them to your watering schedule! </p>
+      <p className="p3">Turning your thumb green since 2024!</p>
     </div>
   );
 }
