58549deb8fea1140235789990053925130a5e05e
zachlengel <156560805+zachlengel@users.noreply.github.com>
2024-05-01 15:09:33 -0400

    changed index headers

--- src/PlantsIndex.jsx
+++ src/PlantsIndex.jsx
@@ -23,24 +23,3 @@ export function PlantsIndex(props) {
     </div>
   );
 }
-
-{
-  /* <article className="card">
-  <img
-    className="card__background"
-    src={plant.image_url}
-    alt={plant.name}
-    width="1920"
-    height="2193"
-  />
-  <div className="card__content | flow">
-    <div className="card__content--container | flow">
-      <h2 className="card__title">{plant.name}</h2>
-      <p className="card__description">
-        {plant.description}
-      </p>
-    </div>
-    <button className="card__button" onClick={() => props.onShowPlant(plant)}>Read more</button>
-  </div>
-</article>; */
-}
