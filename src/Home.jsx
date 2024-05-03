7dd47e55134e8a7940ccc20e4493dbff1e8b5088
zachlengel <156560805+zachlengel@users.noreply.github.com>
2024-05-03 13:17:19 -0400

    added css to homepage

--- src/Home.jsx
+++ src/Home.jsx
@@ -10,17 +10,19 @@ export function Home() {
         <p className="p3">Turning your thumb green since 2024!</p>
       </div>
       <div className="wedo">
-        <h1>What We Do</h1>
-        <p>
+        <h1 className="transition">What We Do</h1>
+        <p className="p1">
           You love your plants, but it can be hard to remember when you last watered or when you need to water again.
         </p>
-        <p>We take care of that for you!</p>
-        <p>Simply find your plants in our list, check how often you need to water and add them to your schedule.</p>
+        <p className="p2">We take care of that for you!</p>
+        <p className="p3">
+          Simply find your plants in our list, check how often you need to water and add them to your schedule.
+        </p>
       </div>
       <div className="water">
-        <h1>How much water do I need to give my plants?</h1>
-        <p>Over-watering can be worse than under-watering!</p>
-        <p>
+        <h1 className="transition">How much water do I need to give my plants?</h1>
+        <p className="p1">Over-watering can be worse than under-watering!</p>
+        <p className="p2">
           Water them until you notice water draining out of the bottom. That means the soil has absorbed all the water
           it can hold.
         </p>
